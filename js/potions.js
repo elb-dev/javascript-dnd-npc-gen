a45c0c367fafa93259f9f0260dffc0c68e9be051
Derek Hendrick <phantom.dev.elb@gmail.com>
2020-09-23 13:24:45 -0700

    Updates most pages and adds rarity and type control

--- js/potions.js
+++ js/potions.js
@@ -6,15 +6,16 @@ function createPotions(){
 	//Second it's the name of the potion,
 	//Third is the price of the potion in gold
 	//Fourth it's the rarity, common is 0, uncommon is 1, rare is 2, very rare is 3, and legendary is 4.
+	//Fifth is if it's a potion or poison. 0 for potion, 1 for poison.
 
 
 	//potions[].push(['','', , ]);
 
 	//Basic Rules
-	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Healing',50,0]);
-	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Greater Healing',150,1]);
-	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Superior Healing',450,2]);
-	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Supreme Healing',1350,3]);
+	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Healing',50,0,0]);
+	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Greater Healing',150,1,0]);
+	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Superior Healing',450,2,0]);
+	potions.push(['https://www.dndbeyond.com/magic-items/potion-of-healing','Potion of Supreme Healing',1350,3,0]);
 
 	return potions;
 }
