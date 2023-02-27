b2064fad96848d9376855606e002e4a03ad44830
Derek Hendrick <phantom.dev.elb@gmail.com>
2023-02-27 16:15:00 -0500

    Adds snare shop

---
+++ js/snares.js
@@ -0,0 +1,39 @@
+function createSnares(){
+	var snares = [];
+
+	//0: link
+	//1: name
+	//2: level
+	//3: price
+	//4: rarity
+
+	//snares.push([link,name,level,price,rarity]);
+
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=328','Alarm Snare',1,3,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=332','Caltrop Snare',1,3,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=336','Hampering Snare',1,3,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=339','Marking Snare',1,3,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=342','Signaling Snare',1,3,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=343','Spike Snare',1,3,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=703','Flare Snare',2,5,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=329','Biting Snare',4,15,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=337','Hobbling Snare',4,15,1]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=344','Stalker Bane Snare',4,15,1]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=347','Trip Snare',4,15,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=348','Warning Snare',4,15,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=704','Nauseating Snare',6,40,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=331','Bomb Snare',8,75,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=334','Grasping Snare',8,75,1]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=345','Striking Snare',8,75,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=705','Snagging Hook Snare',10,180,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=330','Bleeding Spines Snare',12,320,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=341','Scything Blade Snare',12,320,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=346','Stunning Snare',12,320,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=702','Engulfing Snare',14,900,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=335','Hail of Arrows Snare',16,1500,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=340','Omnidirectional Spear Snare',16,1500,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=333','Flying Blade Wheel Snare',20,10000,0]);
+	snares.push(['https://2e.aonprd.com/Equipment.aspx?ID=338','Instant Evisceration Snare',20,10000,0]);
+
+	return snares;
+}
