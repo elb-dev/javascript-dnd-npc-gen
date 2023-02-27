476ac6cb8660dfc8f2063acbd199e2a6c03d8cba
Derek Hendrick <phantom.dev.elb@gmail.com>
2023-02-27 13:47:08 -0500

    Adds new spells

--- screenscrape.js
+++ screenscrape.js
@@ -22,3 +22,54 @@ for(i = 0; i < collectionDivLevel.length; i++){
 	code.push("spells["+levels[i]+"].push(['"+links[i]+"','"+names[i]+"',2]);");
 }
 console.log(code);
+
+
+
+//Div
+var divElements = document.getElementById('ctl00_RadDrawer1_Content_MainContent_DetailedOutput');
+var divChildren = divElements.children
+
+var links = [];
+var names = [];
+var levels = [];
+var rarity = [];
+var currentLevel = -1;
+var spellCounter = -1;
+var htmlStart = 'https://2e.aonprd.com/'
+
+//Items
+var item
+for(i = 0; i < divChildren.length; i++){
+	item = divChildren.item(i)
+	if (item.nodeName == 'H2'){
+		currentLevel += 1;
+	} else if (item.nodeName == 'A'){
+		var itemHref = item.getAttribute('href');
+		if (itemHref != 'PFS.aspx'){
+			spellCounter += 1;
+			levels[spellCounter] = currentLevel;
+			rarity[spellCounter] = 0;
+			links[spellCounter] = htmlStart.concat(itemHref);
+			names[spellCounter] = item.getElementsByTagName('b')[0].getElementsByTagName('u')[0].innerHTML;
+		}
+	} else if (item.nodeName == 'SUP'){
+		if (item.innerHTML == 'U'){
+			rarity[spellCounter] = 1;
+		} else if (item.innerHTML == 'R'){
+			rarity[spellCounter] = 2;
+		} else if (item.innerHTML == 'Q'){
+			rarity[spellCounter] = 3;
+		}
+	}
+}
+
+//Code
+code = [];
+for(i = 0; i < links.length; i++){
+	code[i] = "spells["+levels[i]+"].push(['"+links[i]+"','"+names[i]+"',"+rarity[i]+"]);";
+}
+codeBlock = '';
+for(i = 0; i < code.length; i++){
+	codeBlock = codeBlock.concat(code[i]+'\r\n');
+}
+console.log(codeBlock);
