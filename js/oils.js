71ac4cdd3d7c16209d7b78631b1f7ad7ac267766
Derek Hendrick <phantom.dev.elb@gmail.com>
2023-02-27 15:11:53 -0500

    Adds oil shop

---
+++ js/oils.js
@@ -0,0 +1,65 @@
+function createOils(){
+	var oils = [];
+
+	//0: link
+	//1: name
+	//2: level
+	//3: price
+	//4: rarity
+
+	//oils.push([link,name,level,price,rarity]);
+
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2072','Merciful Balm',1,3,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=174','Nectar of Purification',1,3,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=718','Oil of Unlife (Minor)',1,4,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=724','Shielding Salve',1,4,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2074','Oil of Skating',2,5,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2067','Anticorrosion Oil',2,6,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=181','Oil of Weightlessness',2,6,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=179','Oil of Potency',2,7,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2073','Oil of Ownership (Lesser)',3,8,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2076','Serpent Oil (Lesser)',3,8,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=178','Oil of Mending',3,9,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2069','Disrupting Oil',3,10,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2068','Bane Oil',3,12,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2071','Life-Boosting Oil (Lesser)',3,12,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=718','Oil of Unlife (Lesser)',3,12,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=1563','Anointing Oil',4,18,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=1571','Ghost Oil',4,18,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2076','Serpent Oil (Moderate)',5,24,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=717','Oil of Revelation',5,25,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=183','Salve of Slipperiness',5,25,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2078','Wounding Oil',5,45,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=181','Oil of Weightlessness (Greater)',6,36,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=182','Salve of Antiparalysis',6,40,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2075','Oil of Swiftness',6,50,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=718','Oil of Unlife (Moderate)',6,50,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2073','Oil of Ownership (Moderate)',7,55,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2077','Weapon-Weird Oil',7,55,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2076','Serpent Oil (Greater)',7,56,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2071','Life-Boosting Oil (Moderate)',7,70,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=590','Saints\' Balm',7,70,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=1035','Topology Protoplasm',7,70,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=716','Oil of Object Animation',8,85,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=1022','Ghostly Portal Paint',9,120,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=173','Aligned Oil',9,140,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=1563','Anointing Oil (Greater)',10,180,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=180','Oil of Repulsion',11,175,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2073','Oil of Ownership (Greater)',11,210,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2070','Fundamental Oil',11,220,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2076','Serpent Oil (Major)',11,225,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2069','Disrupting Oil (Greater)',11,250,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=177','Oil of Keen Edges',11,250,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2071','Life-Boosting Oil (Greater)',11,300,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=182','Salve of Antiparalysis (Greater)',12,325,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=176','Oil of Animation',12,330,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=718','Oil of Unlife (Greater)',12,400,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2076','Serpent Oil (True)',15,1100,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=175','Obfuscation Oil',15,1200,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=2071','Life-Boosting Oil (Major)',15,1300,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=716','Oil of Object Animation (Greater)',16,1400,1]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=718','Oil of Unlife (Major)',18,5000,0]);
+	oils.push(['https://2e.aonprd.com/Equipment.aspx?ID=172','Antimagic Oil',20,13000,2]);
+
+	return oils;
+}
