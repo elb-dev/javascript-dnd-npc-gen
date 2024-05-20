e6177ed4126f9fec0296c85fa1484aa6371725ad
Derek Hendrick <phantom.dev.elb@gmail.com>
2024-05-20 12:34:05 -0700

    Adds a number tester function

--- js/functions.js
+++ js/functions.js
@@ -119,6 +119,13 @@ function clamp(min,max,value){
     return value;
 }
 
+//Returns whether or not the string is a number.
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
 
 
 
